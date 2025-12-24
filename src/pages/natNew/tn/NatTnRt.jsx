a1152a974e083a8071634d12fd60272d4a4523be
S-Suwachai <Suwachai.salatip@gmail.com>
2025-12-24 12:31:04 +0700

    fix bug add realtime TN

--- src/pages/natNew/tn/NatTnRt.jsx
+++ src/pages/natNew/tn/NatTnRt.jsx
@@ -22,63 +22,18 @@ export default function NatTnRt() {
   });
   let timeCounter = 0;
 
-  // const dataSource = Array.from({ length: 2 }).map((_, i) => ({
-  //   mc_no:`TB0${i+1}`,
-  //   part_no:"1R-830X10ZZSD718MTTP93",
-  //   tg_pd:"10,234",
-  //   act_pd:"7,000",
-  //   diff_pd:"-3,234",
-  //   // drop:"20",
-  //   act_ct:"1.37",
-  //   diff_ct:"-0.03",
-  //   act_ct_2:"1.37",
-  //   diff_ct_2:"-0.03",
-  //   tg_utl:"100",
-  //   curr_utl:"100",
-  //   // tg_yield:"100",
-  //   // curr_yield:"80",
-  //   status_alarm:"RUNNING",
-  // }));
-
-  // const dataSourcembr = Array.from({ length: 2 }).map((_, i) => ({
-  //   mc_no:`MBR0${i+1}`,
-  //   part_no:"1R-830X10ZZSD718MTTP93",
-  //   f_tg_pd:"10,234",
-  //   f_act_pd:"7,000",
-  //   f_diff_pd:"-3,234",
-  //   f_act_ct:"1.37",
-  //   f_diff_ct:"-0.03",
-  //   f_tg_utl: "100",
-  //   f_curr_utl: "80",
-  //   f_tg_yield: "100",
-  //   f_curr_yield: "80",
-  //   f_status_alarm:"RUNNING",
-  //   s_tg_pd:"10,123",
-  //   s_act_pd:"8,000",
-  //   s_diff_pd:"-2,123",
-  //   s_tg_utl: "100",
-  //   s_curr_utl: "80",
-  //   s_act_ct:"1.02",
-  //   s_diff_ct:"-0.03",
-  //   s_tg_yield: "100",
-  //   s_curr_yield: "80",
-  //   s_status_alarm:"RUNNING",
-  // }));
-
   async function fetchData() {
     if (isFirstLoad.current) {
       setLoading(true);
     }
     try{
-      // const response1 = await axios.get(`${TWN_URL}/nat/tn/tn-realtime/machines`, {
-      const response1 = await axios.get(`http://localhost:8009/nat/tn/tn-realtime/machines`, {
+      const response1 = await axios.get(`${TWN_URL}/nat/tn/tn-realtime/machines`, {
         headers: {
-          "Cache-Control": "no-cache",
-          Pragma: "no-cache",
-          Expires: "0",
+            "Cache-Control": "no-cache",
+            Pragma: "no-cache",
+            Expires: "0",
         },
-      });
-      // console.log(response1.data.data)
+        });
       setDataSourse(response1.data.data);
       setdataSummary(response1.data.resultSummary);
       if (isFirstLoad.current) {
