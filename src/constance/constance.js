4b03f07d8256dcc77afbca419d4b5d1aa0a656bd
S-Suwachai <Suwachai.salatip@gmail.com>
2025-12-29 14:42:53 +0700

    revise error

--- src/constance/constance.js
+++ src/constance/constance.js
@@ -18,6 +18,3 @@ export const LOCAL_URL = "http://10.128.16.110:8009";
 export const NHB_TN_URL = "http://10.120.117.23:3005";
 export const TWN_URL = "http://10.120.139.25:4005";
 // export const TWN_URL = "http://localhost:8009";
-
-export const TEST = "http://localhost:1000";
-export const BASE_URL_NHT_NAT = "http://10.120.139.25:4005"
