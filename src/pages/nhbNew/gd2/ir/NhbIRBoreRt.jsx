6c4d72be878032436fa1226eb40b691906274d58
S-Suwachai <Suwachai.salatip@gmail.com>
2025-12-30 17:06:42 +0700

    add bore nhb

---
+++ src/pages/nhbNew/gd2/ir/NhbIRBoreRt.jsx
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import moment from 'moment';
+import React, { useEffect, useRef, useState } from 'react'
+import Swal from 'sweetalert2';
+import MasterRtPage from '../../../../components/redesign/realtime/MasterRtPage';
+import Loading from '../../../../components/common/Loading';
+import { NHB_GD_URL } from '../../../../constance/constance';
+
+export default function NhbIRBoreRt() {
+    const isFirstLoad = useRef(true);
+    const [loading, setLoading] = useState(false);
+    const [dataSource, setDataSourse] = useState([]);
+    const [dataSummary, setdataSummary] = useState({
+    sum_target: 0,
+    sum_daily_ok: 0,
+    avg_cycle_t: 0,
+    avg_opn: 0,
+    });
+    let timeCounter = 0;
+
+    async function fetchData() {
+    if (isFirstLoad.current) {
+        setLoading(true);
+    }
+    try{
+        const response1 = await axios.get(`${NHB_GD_URL}/nhb/gd/2ndinbore-realtime/machines`, {
+        headers: {
+            "Cache-Control": "no-cache",
+            Pragma: "no-cache",
+            Expires: "0",
+        },
+        });
+        setDataSourse(response1.data.data);
+        setdataSummary(response1.data.resultSummary);
+        if (isFirstLoad.current) {
+            setLoading(false);
+            isFirstLoad.current = false;
+        }
+    }
+    catch (error) {
+        if (isFirstLoad.current) {
+        setLoading(false);
+        isFirstLoad.current = false;
+        }
+        Swal.fire({
+        title: error.message,
+        icon: "warning",
+        timer: 4000,
+        timerProgressBar: true,
+        });
+    }
+    }
+
+    useEffect(() => {
+        fetchData();
+    
+        setInterval(() => {
+            timeCounter = 60 - moment().format("ss");
+            if (timeCounter == 60) {
+                fetchData();
+            }
+            }, 1000);
+        }, []);
+    
+  return (
+    <div>
+        {loading && <Loading />}
+        <MasterRtPage
+            plant_={"NHB"}
+            process_={"I/R Bore"}
+            data={dataSource}
+            dataSum={dataSummary}
+        />
+    </div>
+  )
+}
